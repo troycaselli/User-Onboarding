85557655a77a52658fef12aabcf8011a93bcd43e
Troy Caselli <troycaselli@Troys-MacBook-Air.local>
2023-02-01 09:36:09 -0800

    improved css

--- user-onboarding/src/components/User.js
+++ user-onboarding/src/components/User.js
@@ -1,10 +1,12 @@
+import './User.css';
+
 export default function User(props) {
     const {user} = props;
 
     return (
         <div>
-            <h4>{user.fName} {user.lName} ({user.id})</h4>
-            <p>{user.email}</p>
+            <h4 className='member'>{user.fName} {user.lName} ({user.id})</h4>
+            <p className='email'>{user.email}</p>
         </div>
     )
 }
